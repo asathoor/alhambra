c5da3fca8f24f1d5e88d160d6e2d78e39b44a331
asathoor <asathoor@gmail.com>
2025-10-06 09:55:05 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -20,4 +20,15 @@ add_action(
     'themeslug_enqueue_style' 
 );
 
+// Tilføj Font Awesome CDN til WordPress-temaet
+function petj_enqueue_font_awesome() {
+    wp_enqueue_style(
+        'font-awesome-cdn',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css',
+        array(),
+        '6.5.0'
+    );
+}
+add_action('wp_enqueue_scripts', 'petj_enqueue_font_awesome');
+
 /** Do not add an end PHP tag at the end of this file! */
