39b718bf9bd7af6da200e52944a99b54ca37474c
asathoor <asathoor@gmail.com>
2025-10-09 17:46:02 +0200

    Helt blankt tema
    Tomme headere og footere. En theme.json bestående af et par krøllede parenteser. En udkommenteret functions.php. Blanco della Blanco!

--- functions.php
+++ functions.php
@@ -5,19 +5,25 @@
  */
 
 /** Here we include stylesheets */
+
+/*
 function themeslug_enqueue_style() {
 	wp_enqueue_style( 'petj-theme-styles', 
     get_template_directory_uri() 
     . '/style.css', 
     false );
 }
+    */
 /**
  * wp_enqueue_scripts: will add the script to the header
  * via the function we defined as: themeslug_enqueue_style()
  */
+
+/*
 add_action( 
     'wp_enqueue_scripts', 
     'themeslug_enqueue_style' 
 );
+*/
 
 /** Do not add an end PHP tag at the end of this file! */
